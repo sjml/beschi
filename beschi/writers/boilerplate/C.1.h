cddb3bf51d4860095c615c69f6d50b80611e5873
Shane Liesegang <shane@techie.net>
2022-02-26 08:53:22 +0200

    uf, rookie mistake

--- beschi/writers/boilerplate/C.1.h
+++ beschi/writers/boilerplate/C.1.h
@@ -113,7 +113,7 @@ beschi_err_t beschi__ReadString(beschi_DataAccess *r, char **s, uint32_t *len) {
     if (r->bufferSize < r->position + *len) {
         return BESCHI_ERR_EOF;
     }
-    *s = malloc(*len);
+    *s = calloc(1, *len + 1);
     memcpy(*s, r->buffer + r->position, *len);
     r->position += *len;
     return BESCHI_ERR_OK;
