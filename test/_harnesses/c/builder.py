f845705bca7c5c70e3d8e2c2c77e658fc21b4796
Shane Liesegang <shane@techie.net>
2022-02-27 09:00:35 +0200

    framework for windows tests, not hooked to CI yet - just want to make sure it doesn't break the mac/linux tests first

--- test/_harnesses/c/builder.py
+++ test/_harnesses/c/builder.py
@@ -2,46 +2,74 @@ import os
 import stat
 import platform
 import subprocess
+import shutil
 
 # i hate this
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import builder_util
 
-CC = "clang"
-CXX = "clang++"
+if platform.system() != "Windows":
+    CC = "clang"
+    CXX = "clang++"
 
-FLAGS = [
-    "-pedantic-errors", # complain if compiler extensions come into play
-    "-Weverything",     # actually, complain about everything
-    "-Werror",          # loudly
-    "-O0", "-g",        # have as much debug info as we can
-]
-SILENCE_WARNINGS = [    # turn off these very specific warnings, though
-    "float-equal",      # doing comparisons in the test harness; actually *do* want to to check identical
-    "padded",           # automatically optimizing struct layout is a hard problem :(
-    "unused-parameter", # GetSizeInBytes takes a message pointer, but never looks at it if the size is constant precomputed
-]
-[FLAGS.append(f"-Wno-{sw}") for sw in SILENCE_WARNINGS]
-CFLAGS = [
-    "-std=c99",         # c99 is the baseline
-]
-CSILENCE_WARNINGS = [
-]
-[CFLAGS.append(f"-Wno-{sw}") for sw in CSILENCE_WARNINGS]
+    FLAGS = [
+        "-pedantic-errors", # complain if compiler extensions come into play
+        "-Weverything",     # actually, complain about everything
+        "-Werror",          # loudly
+        "-O0", "-g",        # have as much debug info as we can
+    ]
+    SILENCE_WARNINGS = [    # turn off these very specific warnings, though
+        "float-equal",      # doing comparisons in the test harness; actually *do* want to to check identical
+        "padded",           # automatically optimizing struct layout is a hard problem :(
+        "unused-parameter", # GetSizeInBytes takes a message pointer, but never looks at it if the size is constant precomputed
+    ]
+    [FLAGS.append(f"-Wno-{sw}") for sw in SILENCE_WARNINGS]
+    CFLAGS = [
+        "-std=c99",         # c99 is the baseline
+    ]
+    CSILENCE_WARNINGS = [
+    ]
+    [CFLAGS.append(f"-Wno-{sw}") for sw in CSILENCE_WARNINGS]
 
-CPPFLAGS = [
-    "-std=c++11",       # c++11 is the baseline
-]
-CPPSILENCE_WARNINGS = [
-    # since this code is meant to be usable from C primarily,
-    #   need to silence some warnings that demand more modern C++
-    "c99-extensions", # don't get *that* pedantic
-    "old-style-cast", # let us use c-style casts without complaining
-    "zero-as-null-pointer-constant", # otherwise have to use nullptr, or redefine it, which feels like code smell
-    "cast-qual", # really wish I could only silence this for string literals
-]
-[CPPFLAGS.append(f"-Wno-{sw}") for sw in CPPSILENCE_WARNINGS]
+    CPPFLAGS = [
+        "-x", "c++",        # explicitly compile C files as C++
+        "-std=c++11",       # c++11 is the baseline
+    ]
+    CPPSILENCE_WARNINGS = [
+        # since this code is meant to be usable from C primarily,
+        #   need to silence some warnings that demand more modern C++
+        "c99-extensions", # don't get *that* pedantic
+        "old-style-cast", # let us use c-style casts without complaining
+        "zero-as-null-pointer-constant", # otherwise have to use nullptr, or redefine it, which feels like code smell
+        "cast-qual", # really wish I could only silence this for string literals
+    ]
+    [CPPFLAGS.append(f"-Wno-{sw}") for sw in CPPSILENCE_WARNINGS]
+
+else:
+    CC = "cl.exe"
+    CXX = "cl.exe"
+
+    FLAGS = [
+        "/Wall",        # complain about everything
+        "/WX",          # loudly
+    ]
+    SILENCE_WARNINGS = [  # turn off these very specific warnings, though
+        "4100",           # unused parameter; same notes as above
+        "4820",           # padding; same notes as above
+        "5045",           # /Wall makes a warning about possible spectre mitigation if compiled with a specific flag?
+    ]
+    [FLAGS.append(f"/wd{sw}") for sw in SILENCE_WARNINGS]
+
+    CFLAGS = [
+        "/std:c11",     # <sigh> this is as low as MSVC goes :(
+    ]
+    CPPFLAGS = [
+        "/TP",          # compile C files as C++
+        "/std:c++20",   # required for designated initializers
+    ]
+
+
 
 
 class CBuilder(builder_util.Builder):
@@ -52,44 +80,62 @@ class CBuilder(builder_util.Builder):
         self.exepath_cpp = f"{self.exepath}pp"
         self.intermediate_path = os.path.join(builder_util.INTERMEDIATE_DIR, self.exename)
         self.intermediate_path_cpp = f"{self.intermediate_path}pp"
+        if platform.system() == "Windows":
+            self.exepath += ".exe"
+            self.exepath_cpp += ".exe"
+            self.intermediate_path += ".exe"
+            self.intermediate_path_cpp += ".exe"
 
     def build(self):
         super().build()
 
         build_flags = FLAGS
-        build_flags += [f"-I{os.path.dirname(self.gen_file)}"]
-        if platform.system() == "Darwin":
-            build_flags += ["-isysroot", "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"]
+        if platform.system() == "Windows":
+            build_flags += [f"/I{os.path.dirname(self.gen_file)}"]
+            c_out_flags = [f"/Fe:{self.intermediate_path}", f"/Fo:{self.intermediate_path[:-4]}.obj"]
+            cpp_out_flags = [f"/Fe:{self.intermediate_path_cpp}", f"/Fo:{self.intermediate_path_cpp[:-4]}.obj"]
+        else:
+            build_flags += [f"-I{os.path.dirname(self.gen_file)}"]
+            if platform.system() == "Darwin":
+                build_flags += ["-isysroot", "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk"]
+            c_out_flags = ["-o", self.intermediate_path]
+            cpp_out_flags = ["-o", self.intermediate_path_cpp]
 
         deps = [self.srcfile, self.gen_file, "util.h"]
         if builder_util.needs_build(self.intermediate_path, deps):
             subprocess.check_call([
                 CC, *build_flags,
                 *CFLAGS,
-                "-o", self.intermediate_path,
+                *c_out_flags,
                 self.srcfile
             ])
         if builder_util.needs_build(self.intermediate_path_cpp, deps):
             subprocess.check_call([
                 CXX, *build_flags,
                 *CPPFLAGS,
-                "-o", self.intermediate_path_cpp,
-                "-x", "c++", self.srcfile
+                *cpp_out_flags,
+                self.srcfile
             ])
 
         if builder_util.needs_build(self.exepath, [self.intermediate_path]):
-            shim = open("./exe_template").read()
-            shim = shim.replace("{# EXE_NAME #}", self.exename)
-            with open(self.exepath, "w") as shim_file:
-                shim_file.write(shim)
-            os.chmod(self.exepath, os.stat(self.exepath).st_mode | stat.S_IEXEC)
+            if platform.system() == "Windows":
+                shutil.copy(self.intermediate_path, self.exepath)
+            else:
+                shim = open("./exe_template").read()
+                shim = shim.replace("{# EXE_NAME #}", self.exename)
+                with open(self.exepath, "w") as shim_file:
+                    shim_file.write(shim)
+                os.chmod(self.exepath, os.stat(self.exepath).st_mode | stat.S_IEXEC)
 
         if builder_util.needs_build(self.exepath_cpp, [self.intermediate_path_cpp]):
-            shim = open("./exe_template").read()
-            shim = shim.replace("{# EXE_NAME #}", f"{self.exename}pp")
-            with open(self.exepath_cpp, "w") as shim_file:
-                shim_file.write(shim)
-            os.chmod(self.exepath_cpp, os.stat(self.exepath_cpp).st_mode | stat.S_IEXEC)
+            if platform.system() == "Windows":
+                shutil.copy(self.intermediate_path_cpp, self.exepath_cpp)
+            else:
+                shim = open("./exe_template").read()
+                shim = shim.replace("{# EXE_NAME #}", f"{self.exename}pp")
+                with open(self.exepath_cpp, "w") as shim_file:
+                    shim_file.write(shim)
+                os.chmod(self.exepath_cpp, os.stat(self.exepath_cpp).st_mode | stat.S_IEXEC)
 
 
 
